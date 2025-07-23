5fc3fa67de2bc1a42e856557ce6e5ff140652a0b
gregor <gjkcme@gmail.com>
2025-07-23 14:05:01 +0200

    Create documents.tsx
    analitika

---
+++ pages/documents.tsx
@@ -0,0 +1,32 @@
+// pages/_document.tsx
+
+import Document, { Html, Head, Main, NextScript } from 'next/document';
+
+class MyDocument extends Document {
+  render() {
+    return (
+      <Html lang="sl">
+        <Head>
+          {/* Google Analytics */}
+          <script async src="https://www.googletagmanager.com/gtag/js?id=G-5VVENQ6E2G"></script>
+          <script
+            dangerouslySetInnerHTML={{
+              __html: `
+                window.dataLayer = window.dataLayer || [];
+                function gtag(){dataLayer.push(arguments);}
+                gtag('js', new Date());
+                gtag('config', 'G-5VVENQ6E2G');
+              `,
+            }}
+          />
+        </Head>
+        <body>
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
+    );
+  }
+}
+
+export default MyDocument;
