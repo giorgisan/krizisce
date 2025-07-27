af6445fb3f71e0fda199d739063092db8f3882af
gregor <gjkcme@gmail.com>
2025-07-27 19:12:34 +0200

    Update tailwind.config.js
    no tail

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,15 @@
+// tailwind.config.js
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   darkMode: 'class',
   content: ['./pages/**/*.{js,ts,jsx,tsx}', './components/**/*.{js,ts,jsx,tsx}'],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        brand: '#fc946c',
+        'brand-hover': '#e57b53',
+      },
+    },
   },
   plugins: [],
 }
