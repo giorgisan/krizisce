7ff7cdc2c996090c26846ee72fd3ee776cfd3b05
gregor <gjkcme@gmail.com>
2025-07-30 14:05:43 +0200

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,4 +1,4 @@
-/* tailwind.config.js */
+// tailwind.config.js
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   darkMode: 'class',
@@ -8,13 +8,6 @@ module.exports = {
       colors: {
         brand: '#fc9c6c',
         'brand-hover': '#e57b53',
-        secondary: '#3B82F6',
-        success: '#10B981',
-        error: '#DC2626',
-        'bg-light': '#F5F5F5',
-        'bg-dark': '#0D0D0D',
-        'text-light': '#1F2937',
-        'text-dark': '#E5E7EB',
       },
     },
   },
