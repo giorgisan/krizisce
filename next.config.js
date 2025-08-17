a8ac526cda9ee51e3e5a7a93151422ecf058319f
gregor <gjkcme@gmail.com>
2025-08-17 17:46:51 +0200

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,27 +1,11 @@
 // next.config.js
-
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  reactStrictMode: true,
+module.exports = {
   images: {
-    domains: [
-      'img.24ur.com',
-      'siol.net',
-      'www.rtvslo.si',
-      'static.24ur.com',
-      'www.delo.si',
-      'img.rtvslo.si',
-      'siol.slovenskenovice.si',
-      'media.siol.net',
-      'storage.googleapis.com',
-      'www.svet24.si',
-      'cdn.24ur.com',
-      'slovenskenovice.si',
-      'www.n1info.si',
-      'si.bloombergadria.com',
-      'forbes.n1info.si',
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: '**', // dovolimo vse domene (ali pa nastaviš točno določene npr. 'www.24ur.com')
+      },
     ],
   },
 }
-
-module.exports = nextConfig
