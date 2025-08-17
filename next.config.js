d56a52a179d3cc2851c4e13b1259ef8b79165b20
gregor <gjkcme@gmail.com>
2025-08-17 17:45:07 +0200

    Create next.config.js

---
+++ next.config.js
@@ -0,0 +1,27 @@
+// next.config.js
+
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  reactStrictMode: true,
+  images: {
+    domains: [
+      'img.24ur.com',
+      'siol.net',
+      'www.rtvslo.si',
+      'static.24ur.com',
+      'www.delo.si',
+      'img.rtvslo.si',
+      'siol.slovenskenovice.si',
+      'media.siol.net',
+      'storage.googleapis.com',
+      'www.svet24.si',
+      'cdn.24ur.com',
+      'slovenskenovice.si',
+      'www.n1info.si',
+      'si.bloombergadria.com',
+      'forbes.n1info.si',
+    ],
+  },
+}
+
+module.exports = nextConfig
