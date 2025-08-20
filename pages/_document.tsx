ffa18883a301118625e3b5b4ad2c396494dbee2c
gregor <gjkcme@gmail.com>
2025-08-20 14:07:10 +0200

    Update _document.tsx

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,5 +1,4 @@
 // pages/_document.tsx
-
 import Document, { Html, Head, Main, NextScript } from 'next/document'
 
 class MyDocument extends Document {
@@ -7,29 +6,17 @@ class MyDocument extends Document {
     return (
       <Html lang="sl">
         <Head>
-          {/* Prikaz faviconov za vse brskalnike */}
+          {/* Favicone */}
           <link rel="icon" href="/favicon.ico" sizes="any" />
           <link rel="icon" type="image/png" href="/logo.png" />
-          {/* Apple Touch Icon za iOS (180×180 px) */}
           <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
 
+          {/* Barve UI */}
           <meta name="theme-color" content="#0f172a" />
           <meta name="msapplication-TileColor" content="#0f172a" />
-
-          {/* Google Analytics – enoten ID */}
-          <script async src="https://www.googletagmanager.com/gtag/js?id=G-5VVENQ6E2G"></script>
-          <script
-            dangerouslySetInnerHTML={{
-              __html: `
-                window.dataLayer = window.dataLayer || [];
-                function gtag(){dataLayer.push(arguments);}
-                gtag('js', new Date());
-                gtag('config', 'G-5VVENQ6E2G');
-              `,
-            }}
-          />
         </Head>
-        <body className="bg-gray-900 text-white">
+        {/* Osnovno dark ozadje; ThemeProvider bo dodal/odstranil .dark na <html> */}
+        <body className="bg-gray-900 text-white antialiased">
           <Main />
           <NextScript />
         </body>
