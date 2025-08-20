dd8a5e71163cad74ae37904e6a7eea9cfe7f1094
gregor <gjkcme@gmail.com>
2025-08-20 20:10:40 +0200

    Update _app.tsx

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -3,8 +3,8 @@ import '../styles/globals.css'
 import type { AppProps } from 'next/app'
 import { useEffect } from 'react'
 import { useRouter } from 'next/router'
-import Head from 'next/head'
 import { ThemeProvider } from 'next-themes'
+import Script from 'next/script'
 
 // Vercel Analytics & Speed Insights
 import { Analytics } from '@vercel/analytics/next'
@@ -18,45 +18,54 @@ declare global {
   }
 }
 
-// Enoten GA ID
+// GA4 ID
 const GA_MEASUREMENT_ID = 'G-5VVENQ6E2G'
+
+// Pomagalna funkcija za SPA page_view
+const pageview = (url: string) => {
+  if (typeof window.gtag === 'function') {
+    window.gtag('config', GA_MEASUREMENT_ID, { page_path: url })
+  }
+}
 
 function App({ Component, pageProps }: AppProps) {
   const router = useRouter()
 
   useEffect(() => {
-    const handleRouteChange = (url: string) => {
-      if (typeof window.gtag === 'function') {
-        window.gtag('config', GA_MEASUREMENT_ID, { page_path: url })
-      }
-    }
+    const handleRouteChange = (url: string) => pageview(url)
     router.events.on('routeChangeComplete', handleRouteChange)
     return () => router.events.off('routeChangeComplete', handleRouteChange)
   }, [router.events])
 
   return (
     <>
-      {/* Google Analytics */}
-      <Head>
-        <script
-          async
-          src={`https://www.googletagmanager.com/gtag/js?id=${GA_MEASUREMENT_ID}`}
-        />
-        <script
-          id="ga-init"
-          dangerouslySetInnerHTML={{
-            __html: `
-              window.dataLayer = window.dataLayer || [];
-              function gtag(){dataLayer.push(arguments);}
-              gtag('js', new Date());
-              gtag('config', '${GA_MEASUREMENT_ID}');
-            `,
-          }}
-        />
-      </Head>
+      {/* GA4 – neblokirajoče nalaganje */}
+      <Script
+        id="ga4-lib"
+        src={`https://www.googletagmanager.com/gtag/js?id=${GA_MEASUREMENT_ID}`}
+        strategy="afterInteractive"
+      />
+      <Script
+        id="ga4-init"
+        strategy="afterInteractive"
+        dangerouslySetInnerHTML={{
+          __html: `
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', '${GA_MEASUREMENT_ID}', { anonymize_ip: true });
+          `,
+        }}
+      />
 
-      {/* Privzeto DARK, brez system override; attribute="class" za Tailwind */}
-      <ThemeProvider attribute="class" defaultTheme="dark" enableSystem={false}>
+      {/* Tema: privzeto DARK, brez system override; class način za Tailwind */}
+      <ThemeProvider
+        attribute="class"
+        defaultTheme="dark"
+        enableSystem={false}
+        storageKey="theme"
+        disableTransitionOnChange
+      >
         <Component {...pageProps} />
       </ThemeProvider>
 
