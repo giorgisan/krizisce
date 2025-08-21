ccb26907549483f7d647a22e2595d2a0d8d1de2d
gregor <gjkcme@gmail.com>
2025-08-21 11:50:05 +0200

    Update ArticleCard.tsx

--- components/ArticleCard.tsx
+++ components/ArticleCard.tsx
@@ -71,7 +71,8 @@ export default function ArticleCard({ news }: Props) {
             className="absolute top-2 right-2 p-2 rounded-full 
                        bg-white/80 dark:bg-gray-900/70 text-gray-700 dark:text-gray-200 
                        shadow-sm opacity-0 group-hover:opacity-100 
-                       transition-all duration-200 hover:bg-white dark:hover:bg-gray-800"
+                       transform transition duration-200 
+                       hover:scale-110 hover:bg-white dark:hover:bg-gray-800"
           >
             <svg
               xmlns="http://www.w3.org/2000/svg"
