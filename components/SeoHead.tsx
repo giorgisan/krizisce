22bf68b1c9c45cf187b257629db984248a364ac3
gregor <gjkcme@gmail.com>
2025-08-23 15:07:15 +0200

    Update SeoHead.tsx

--- components/SeoHead.tsx
+++ components/SeoHead.tsx
@@ -21,16 +21,6 @@ export default function SeoHead({
   const fullTitle = title === 'Križišče' ? 'Križišče' : `${title} · Križišče`
   const ogImage = image.startsWith('http') ? image : new URL(image, url).toString()
 
-  // preconnect na Supabase, če obstaja env
-  const supabaseHost = (() => {
-    try {
-      const u = process.env.NEXT_PUBLIC_SUPABASE_URL
-      return u ? new URL(u).hostname : ''
-    } catch {
-      return ''
-    }
-  })()
-
   const fallbackJsonLd = {
     '@context': 'https://schema.org',
     '@type': 'WebSite',
@@ -69,6 +59,7 @@ export default function SeoHead({
       <meta property="og:image" content={ogImage} />
       <meta property="og:image:width" content="1200" />
       <meta property="og:image:height" content="630" />
+      <meta property="og:image:alt" content="Križišče – predogledna slika" />
       <meta property="og:locale" content="sl_SI" />
 
       {/* Twitter */}
@@ -84,36 +75,8 @@ export default function SeoHead({
         dangerouslySetInnerHTML={{ __html: JSON.stringify(jsonLd ?? fallbackJsonLd) }}
       />
 
-      {/* ---- Performance add-ons (varno) ---- */}
-      {/* DNS prefetch omogoči */}
-      <meta httpEquiv="x-dns-prefetch-control" content="on" />
-
-      {/* Supabase (če ga uporabljaš za /api/click ipd.) */}
-      {supabaseHost && (
-        <>
-          <link rel="dns-prefetch" href={`//${supabaseHost}`} />
-          <link rel="preconnect" href={`https://${supabaseHost}`} crossOrigin="anonymous" />
-        </>
-      )}
-
-      {/* Google Tag Manager / Analytics (imaš GTM snippet) */}
-      <link rel="dns-prefetch" href="//www.googletagmanager.com" />
-      <link rel="preconnect" href="https://www.googletagmanager.com" crossOrigin="anonymous" />
-      <link rel="dns-prefetch" href="//www.google-analytics.com" />
-      <link rel="preconnect" href="https://www.google-analytics.com" crossOrigin="anonymous" />
-
-      {/* Google Fonts (če jih uporabljaš; harmless, če ne) */}
-      <link rel="dns-prefetch" href="//fonts.googleapis.com" />
-      <link rel="preconnect" href="https://fonts.googleapis.com" />
-      <link rel="dns-prefetch" href="//fonts.gstatic.com" />
-      <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
-
-      {/* Preload logotipa, ki je v headerju */}
+      {/* Preload logotipa v headerju */}
       <link rel="preload" href="/logo.png" as="image" />
-
-      {/* Barva naslovne vrstice na mobilnih napravah */}
-      <meta name="theme-color" content="#0d0d0d" />
-      <meta name="color-scheme" content="light dark" />
     </Head>
   )
 }
