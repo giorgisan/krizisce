49c5f4d7b26456dfa5b926d695dea690b599b6c0
gregor <gjkcme@gmail.com>
2025-08-23 09:41:44 +0200

    Update next.config.js
    optimizacija

--- next.config.js
+++ next.config.js
@@ -1,11 +1,16 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-
-  // Ugasne Vercel/Next image optimization globalno
-  images: {
-    unoptimized: true,
+  async headers() {
+    return [
+      {
+        // velja za static iz /public (npr. /logos/*, /logo.png ...)
+        source: '/:all*\\.(png|jpg|jpeg|gif|webp|avif|svg)$',
+        headers: [
+          { key: 'Cache-Control', value: 'public, max-age=31536000, immutable' },
+        ],
+      },
+    ]
   },
-};
-
-module.exports = nextConfig;
+}
+module.exports = nextConfig
