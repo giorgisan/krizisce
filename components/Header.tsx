b892d02f92b818992ea9a4e773a052778bc0d70a
gregor <gjkcme@gmail.com>
2025-12-13 12:22:19 +0100

    Update Header.tsx

--- components/Header.tsx
+++ components/Header.tsx
@@ -180,7 +180,7 @@ export default function Header({
                   </div>
                   <input
                     type="search"
-                    placeholder="Išči po naslovu ali vsebini..."
+                    placeholder="Išči po naslovu ali podnaslovu ..."
                     className="block w-full pl-10 pr-3 py-1.5 bg-gray-100 dark:bg-gray-800 border border-transparent 
                               focus:bg-white dark:focus:bg-black focus:border-brand/30 focus:ring-2 focus:ring-brand/10
                               rounded-md text-sm transition-all placeholder-gray-500 text-gray-900 dark:text-white"
