50b22dadc94612ecedbc7f1b86b9655ff1e0b437
gregor <gjkcme@gmail.com>
2025-12-15 12:33:24 +0100

    Update _app.tsx

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,63 +1,16 @@
 // pages/_app.tsx
 import '../styles/globals.css'
 import type { AppProps } from 'next/app'
-import { useEffect } from 'react'
-import { useRouter } from 'next/router'
 import { ThemeProvider } from 'next-themes'
-import Script from 'next/script'
 
-// Vercel Analytics & Speed Insights
+// Vercel Analytics & Speed Insights 
+// (Privacy friendly - brez piškotkov, zato ne rabiš bannerja)
 import { Analytics } from '@vercel/analytics/next'
 import { SpeedInsights } from '@vercel/speed-insights/next'
 
-// TypeScript naj pozna window.gtag
-declare global {
-  interface Window {
-    gtag: (...args: any[]) => void
-    dataLayer?: any[]
-  }
-}
-
-// GA4 ID
-const GA_MEASUREMENT_ID = 'G-5VVENQ6E2G'
-
-// Pomagalna funkcija za SPA page_view
-const pageview = (url: string) => {
-  if (typeof window.gtag === 'function') {
-    window.gtag('config', GA_MEASUREMENT_ID, { page_path: url })
-  }
-}
-
 function App({ Component, pageProps }: AppProps) {
-  const router = useRouter()
-
-  useEffect(() => {
-    const handleRouteChange = (url: string) => pageview(url)
-    router.events.on('routeChangeComplete', handleRouteChange)
-    return () => router.events.off('routeChangeComplete', handleRouteChange)
-  }, [router.events])
-
   return (
     <>
-      {/* GA4 – neblokirajoče nalaganje */}
-      <Script
-        id="ga4-lib"
-        src={`https://www.googletagmanager.com/gtag/js?id=${GA_MEASUREMENT_ID}`}
-        strategy="afterInteractive"
-      />
-      <Script
-        id="ga4-init"
-        strategy="afterInteractive"
-        dangerouslySetInnerHTML={{
-          __html: `
-            window.dataLayer = window.dataLayer || [];
-            function gtag(){dataLayer.push(arguments);}
-            gtag('js', new Date());
-            gtag('config', '${GA_MEASUREMENT_ID}', { anonymize_ip: true });
-          `,
-        }}
-      />
-
       {/* Tema: privzeto DARK, brez system override; class način za Tailwind */}
       <ThemeProvider
         attribute="class"
@@ -69,8 +22,10 @@ function App({ Component, pageProps }: AppProps) {
         <Component {...pageProps} />
       </ThemeProvider>
 
-      {/* Vercel metrika */}
+      {/* Vercel metrika - anonimna statistika obiska */}
       <Analytics />
+      
+      {/* Vercel Speed Insights - merjenje hitrosti strani */}
       <SpeedInsights />
     </>
   )
