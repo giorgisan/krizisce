9ca8d0d3883ff7b584f1ade82077039ca6cf504e
gregor <gjkcme@gmail.com>
2025-12-24 12:18:13 +0100

    Update index.tsx

--- pages/index.tsx
+++ pages/index.tsx
@@ -298,8 +298,8 @@ export default function Home({ initialNews, initialTrendingWords }: Props) {
   }
 
   const gridClasses = mode === 'trending'
-      ? 'grid gap-6 grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5'
-      : 'grid gap-6 grid-cols-2 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5'
+      ? 'grid gap-4 md:gap-6 grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5'
+      : 'grid gap-4 md:gap-6 grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5'
 
   const activeSourceLabel = selectedSources.length === 0 
     ? 'Vse' 
