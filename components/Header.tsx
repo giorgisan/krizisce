6b8b069d72c7fe8484f25947a5e799eaf7d526c5
gregor <gjkcme@gmail.com>
2026-02-14 19:39:15 +0100

    Update Header.tsx

--- components/Header.tsx
+++ components/Header.tsx
@@ -239,7 +239,7 @@ export default function Header({
 
           {/* --- 2. SREDINA (MOBILE - CENTRIRANO) & LEVO (DESKTOP) --- */}
           <div className="absolute left-1/2 -translate-x-1/2 md:static md:translate-x-0 md:flex md:items-center md:gap-4 md:mr-auto z-0 flex flex-col items-center md:flex-row md:items-center text-center md:text-left">
-            <Link href="/" onClick={handleLogoClick} className="flex flex-col md:flex-row items-center gap-1 md:gap-3 group">
+            <Link href="/" onClick={handleLogoClick} className="flex flex-col md:flex-row items-center md:items-end gap-1 md:gap-3 group">
                 <div className="flex items-center gap-2">
                     <div className="relative w-7 h-7 md:w-9 md:h-9">
                       <Image src="/logo.png" alt="Logo" fill className="object-contain" />
